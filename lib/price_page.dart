9c35acf67314d1687b15d3a9e204ce5070c639b3
42kangaroo <yael.arn@bluewin.ch>
2022-09-07 12:10:54 +0200

    make price-page scrollable

--- lib/price_page.dart
+++ lib/price_page.dart
@@ -24,81 +24,83 @@ class _PricePageState extends State<PricePage> {
       // 2
       body: SafeArea(
         // 3
-        child: Column(
-          children: [
-            GestureDetector(
-              onTap: () {
-                Querry updatedQuerry = widget.querry;
-                updatedQuerry.p = price.LOW;
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return ResultPage(querry: updatedQuerry);
-                }));
-              },
-              child: Card(
-                child: Column(
-                  children: const [
-                    Icon(
-                      Icons.attach_money,
-                      size: 200,
-                    ),
-                    Text(
-                      "Low",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  Querry updatedQuerry = widget.querry;
+                  updatedQuerry.p = price.LOW;
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return ResultPage(querry: updatedQuerry);
+                  }));
+                },
+                child: Card(
+                  child: Column(
+                    children: const [
+                      Icon(
+                        Icons.attach_money,
+                        size: 200,
+                      ),
+                      Text(
+                        "Low",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            GestureDetector(
-              onTap: () {
-                Querry updatedQuerry = widget.querry;
-                updatedQuerry.p = price.MIDDLE;
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return ResultPage(querry: updatedQuerry);
-                }));
-              },
-              child: Card(
-                child: Column(
-                  children: const [
-                    Icon(
-                      Icons.money,
-                      size: 200,
-                    ),
-                    Text(
-                      "Medium",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ],
+              GestureDetector(
+                onTap: () {
+                  Querry updatedQuerry = widget.querry;
+                  updatedQuerry.p = price.MIDDLE;
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return ResultPage(querry: updatedQuerry);
+                  }));
+                },
+                child: Card(
+                  child: Column(
+                    children: const [
+                      Icon(
+                        Icons.money,
+                        size: 200,
+                      ),
+                      Text(
+                        "Medium",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            GestureDetector(
-              onTap: () {
-                Querry updatedQuerry = widget.querry;
-                updatedQuerry.p = price.NO_RESTRICTION;
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return ResultPage(querry: updatedQuerry);
-                }));
-              },
-              child: Card(
-                child: Column(
-                  children: const [
-                    Icon(
-                      Icons.price_change_outlined,
-                      size: 200,
-                    ),
-                    Text(
-                      "No Restriction",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ],
+              GestureDetector(
+                onTap: () {
+                  Querry updatedQuerry = widget.querry;
+                  updatedQuerry.p = price.NO_RESTRICTION;
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return ResultPage(querry: updatedQuerry);
+                  }));
+                },
+                child: Card(
+                  child: Column(
+                    children: const [
+                      Icon(
+                        Icons.price_change_outlined,
+                        size: 200,
+                      ),
+                      Text(
+                        "No Restriction",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              width: double.infinity,
-            ),
-          ],
+              const SizedBox(
+                width: double.infinity,
+              ),
+            ],
+          ),
         ),
       ),
     );
