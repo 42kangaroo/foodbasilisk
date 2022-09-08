12444f2293f411c3221bf7a7520c07fb18f0d135
42kangaroo <yael.arn@bluewin.ch>
2022-09-08 18:56:36 +0200

    Only show Restaurants matching query

--- lib/result_page.dart
+++ lib/result_page.dart
@@ -1,8 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'detail_page.dart';
 import 'restaurant.dart';
 
-import 'settings.dart';
 import 'querry.dart';
 
 class ResultPage extends StatefulWidget {
@@ -20,6 +20,8 @@ class ResultPage extends StatefulWidget {
 }
 
 class _ResultPageState extends State<ResultPage> {
+  List<Restaurant> res = [];
+
   Card createCard(Restaurant resto) {
     return Card(
       child: Column(
@@ -34,27 +36,47 @@ class _ResultPageState extends State<ResultPage> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Choose Restaurant"),
-      ),
-      body: SafeArea(
-        child: ListView.builder(
-          itemCount: restaurants.length,
-          itemBuilder: (BuildContext context, int index) {
-            return GestureDetector(
-              onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return RestaurantDetail(restaurant: restaurants[index]);
-                }));
-              },
-              child: createCard(restaurants[index]),
-            );
-          },
-        ),
-      ),
-    );
+  Future<void> getData() async {
+    res.clear();
+    FirebaseFirestore db = FirebaseFirestore.instance;
+    List<List<int>> helperPrice = [[0], [0,1],[0,1,2]];
+    await db
+        .collection("restaurants")
+        .where("categories", arrayContains: widget.querry.category)
+        .where("distance", isLessThanOrEqualTo: widget.querry.distance)
+        .where("price", whereIn: helperPrice[widget.querry.p.index])
+        .get()
+        .then((value) {
+      for (var element in value.docs) {
+        res.add(Restaurant.fromMap(element.data()));
+      }
+    });
   }
+
+  @override
+  Widget build(BuildContext context) => FutureBuilder(
+      future: getData(),
+      builder: (context, snapshot) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("Choose Restaurant"),
+          ),
+          body: SafeArea(
+            child: ListView.builder(
+              itemCount: res.length,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return RestaurantDetail(restaurant: res[index]);
+                    }));
+                  },
+                  child: createCard(res[index]),
+                );
+              },
+            ),
+          ),
+        );
+      });
 }
