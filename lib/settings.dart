dcb6ec54c37a67d5b21363b2bb88bde41145780a
BloodMoonInformatics <tim.plueckebaum@icloud.com>
2022-09-16 08:31:27 +0200

    Changed order of categories

--- lib/settings.dart
+++ lib/settings.dart
@@ -3,15 +3,15 @@ import 'package:flutter/material.dart';
 import 'category.dart';
 
 List<EatCategory> categories = List<EatCategory>.from([
-  EatCategory(AssetImage("assets/donut.png"), "Sweets"),
   EatCategory(AssetImage("assets/pizza.png"), "Italian"),
   EatCategory(AssetImage("assets/hamburger (1).png"), "Burger"),
-  EatCategory(AssetImage("assets/avocado.png"), "Mexican"),
   EatCategory(AssetImage("assets/sushi.png"), "Japanese"),
-  EatCategory(AssetImage("assets/trolley-cart.png"), "Supermarket"),
+  EatCategory(AssetImage("assets/avocado.png"), "Mexican"),
   EatCategory(AssetImage("assets/thai-food.png"), "Thai"),
   EatCategory(AssetImage("assets/chinese-food.png"), "Chinese"),
-  EatCategory(AssetImage("assets/doner-kebab (1).png"), "Döner"),
-  EatCategory(AssetImage("assets/fried-chicken.png"), "Fastfood"),
   EatCategory(AssetImage("assets/vegetarian.png"), "Vegetarian"),
+  EatCategory(AssetImage("assets/fried-chicken.png"), "Fastfood"),
+  EatCategory(AssetImage("assets/doner-kebab (1).png"), "Döner"),
+  EatCategory(AssetImage("assets/trolley-cart.png"), "Supermarket"),
+  EatCategory(AssetImage("assets/donut.png"), "Sweets"),
 ]);
